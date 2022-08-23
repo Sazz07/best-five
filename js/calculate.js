312cd50cf0c52230b89906e88f6a89de179036f8
Sazz07 <sazzad.hossain882@gmail.com>
2022-08-23 13:58:41 +0600

    total expenses calculation done

--- js/calculate.js
+++ js/calculate.js
@@ -10,8 +10,24 @@ function getElementValueById(idElement) {
 // Player Expenses
 
 document.getElementById('btn-calculator').addEventListener('click', function () {
+ 
     const perPlayerCost = getElementValueById('per-player-cost');
     const playerCost = perPlayerCost * playername.length; 
     const playerExpenses = document.getElementById('player-expenses')
     playerExpenses.innerText = playerCost;
+});
+
+// Total Expenses calculation 
+
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+
+    const perPlayerCost = getElementValueById('per-player-cost');
+    const playerCost = perPlayerCost * playername.length; 
+
+    const managerExpenses = getElementValueById('manager-amount-field');
+    const coachExpenses = getElementValueById('coach-amount-field');
+    const totalBudget = managerExpenses + coachExpenses + playerCost;
+
+    const myTotal = document.getElementById('total-cost');
+    myTotal.innerText = totalBudget;
 });
